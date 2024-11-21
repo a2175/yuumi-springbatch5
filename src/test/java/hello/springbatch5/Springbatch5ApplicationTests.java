7ef73a2124beaed05b1ba0a918419ce9950e35bf
a2175 <as217512@gmail.com>
2024-11-21 22:21:43 +0900

    init

---
+++ src/test/java/hello/springbatch5/Springbatch5ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbatch5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbatch5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
