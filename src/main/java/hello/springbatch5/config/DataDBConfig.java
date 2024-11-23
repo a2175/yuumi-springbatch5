149d32c6a2891f7f0d73c2d33529fb1974fcb710
a2175 <as217512@gmail.com>
2024-11-23 19:50:43 +0900

    스프링 배치 5 : 4. DB 연결 Config 클래스
    https://www.youtube.com/watch?v=OfMM4BbBlpY&list=PLJkjrxxiBSFCaxkvfuZaK5FzqQWJwmTfR&index=4

---
+++ src/main/java/hello/springbatch5/config/DataDBConfig.java
@@ -0,0 +1,57 @@
+package hello.springbatch5.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableJpaRepositories(
+    basePackages = "hello.springbatch5.repository",
+    entityManagerFactoryRef = "dataEntityManagerFactoryBean",
+    transactionManagerRef = "dataTransactionManager"
+)
+public class DataDBConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource-data")
+    public DataSource dataDBSource() {
+
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean dataEntityManagerFactoryBean() {
+
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataDBSource());
+        factoryBean.setPackagesToScan(new String[]{"hello.springbatch5.entity"});
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.format_sql", "true");
+        factoryBean.setJpaPropertyMap(properties);
+
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager dataTransactionManager() {
+
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+
+        transactionManager.setEntityManagerFactory(dataEntityManagerFactoryBean().getObject());
+
+        return transactionManager;
+    }
+}
