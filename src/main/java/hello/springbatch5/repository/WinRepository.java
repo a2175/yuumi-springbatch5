3b73443b26d05dbc0e4e7504c8e89038c0ebbd56
a2175 <as217512@gmail.com>
2024-11-27 18:40:16 +0900

    스프링 배치 5 : 8. 배치 처리2 : 테이블 조건
    https://www.youtube.com/watch?v=X1lGjIeNHF8&list=PLJkjrxxiBSFCaxkvfuZaK5FzqQWJwmTfR&index=8

---
+++ src/main/java/hello/springbatch5/repository/WinRepository.java
@@ -0,0 +1,11 @@
+package hello.springbatch5.repository;
+
+import hello.springbatch5.entity.WinEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WinRepository extends JpaRepository<WinEntity, Long> {
+
+    Page<WinEntity> findByWinGreaterThanEqual(Long win, Pageable pageable);
+}
